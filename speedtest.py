6e655294115c4770267d47b1b0cf29209b51f42e
Kenneth Rose <rosekenny12@gmail.com>
2022-09-06 22:23:58 -0400

    Add prometheus client to serve data

--- speedtest.py
+++ speedtest.py
@@ -1,15 +1,27 @@
 import json
 import subprocess
+from time import sleep
+from prometheus_client import Gauge, start_http_server
 
+# Init prometheus server to post metrics
+start_http_server(9191)
 
-# Run speedtest
-# Specify json output
-# Pipe output of subprocess to variable
-speedtest_results = subprocess.Popen(['speedtest.exe', '-f', 'json'], stdout=subprocess.PIPE).communicate()[0]
-json_results = json.loads(speedtest_results)
+# Init prometheus gauges for each data point
+prom_latency = Gauge('speedtest_latency', 'Latency of the connection to the test server')
+prom_download = Gauge('speedtest_download', 'Download speed in bytes of the internet connection')
+prom_upload = Gauge('speedtest_upload', 'Upload speed in bytes of the internet connection')
 
-# Scrape relevant data
-avg_latency = json_results['ping']['latency']
-download_speed = json_results['download']['bandwidth']
-upload_speed = json_results['upload']['bandwidth']
-packet_loss = json_results['packetLoss']
+while True:
+    
+    # Run speedtest
+    # Specify json output
+    # Pipe output of subprocess to variable
+    speedtest_results = subprocess.Popen(['speedtest.exe', '-f', 'json'], stdout=subprocess.PIPE).communicate()[0]
+    json_results = json.loads(speedtest_results)
+
+    # Scrape and post relevant data
+    prom_latency.set(json_results['ping']['latency'])
+    prom_download.set(json_results['download']['bandwidth'])
+    prom_upload.set(json_results['upload']['bandwidth'])
+
+    sleep(60)
