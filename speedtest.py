78cb9b7ab96353830a0e3efaffed39bf8980a46c
Kenneth Rose <rosekenny12@gmail.com>
2022-09-07 21:09:24 -0400

    Added ping tests

--- speedtest.py
+++ speedtest.py
@@ -1,7 +1,8 @@
 import json
 import subprocess
+import threading
 from time import sleep
-from prometheus_client import Gauge, start_http_server
+from prometheus_client import Gauge, Counter, start_http_server
 
 # Init prometheus server to post metrics
 start_http_server(9191)
@@ -10,18 +11,63 @@ start_http_server(9191)
 prom_latency = Gauge('speedtest_latency', 'Latency of the connection to the test server')
 prom_download = Gauge('speedtest_download', 'Download speed in bytes of the internet connection')
 prom_upload = Gauge('speedtest_upload', 'Upload speed in bytes of the internet connection')
+prom_ping_success = Counter('ping_success', 'Number of successful pings')
+prom_ping_fails = Counter('ping_fails', 'Number of failed pings')
 
-while True:
-    
-    # Run speedtest
-    # Specify json output
-    # Pipe output of subprocess to variable
-    speedtest_results = subprocess.Popen(['speedtest.exe', '-f', 'json'], stdout=subprocess.PIPE).communicate()[0]
-    json_results = json.loads(speedtest_results)
 
-    # Scrape and post relevant data
-    prom_latency.set(json_results['ping']['latency'])
-    prom_download.set(json_results['download']['bandwidth'])
-    prom_upload.set(json_results['upload']['bandwidth'])
+def speedtest():
 
-    sleep(60)
+    global kill_threads
+
+    while not kill_threads:
+        
+        # Run speedtest
+        # Specify json output
+        # Pipe output of subprocess to variable
+        speedtest_results = subprocess.Popen(['speedtest.exe', '-f', 'json'], stdout=subprocess.PIPE).communicate()[0]
+        json_results = json.loads(speedtest_results)
+
+        # Scrape and post relevant data
+        prom_latency.set(json_results['ping']['latency'])
+        prom_download.set(json_results['download']['bandwidth'])
+        prom_upload.set(json_results['upload']['bandwidth'])
+
+        sleep(60)
+
+
+def ping_test(servers):
+
+    global kill_threads
+
+    while not kill_threads:
+
+        for server in servers:
+
+            # Send a single ping to the current server and capture result by piping output
+            ping_result = subprocess.Popen(['ping', '/n', '1', server], stdout=subprocess.PIPE).communicate()[0]
+            
+            if 'Received = 1' in str(ping_result):
+                prom_ping_success.inc()
+            else:
+                prom_ping_fails.inc()
+
+        sleep(10)
+
+
+ping_thread = threading.Thread(target=ping_test, args=(['1.1.1.1','8.8.8.8'],))
+speedtest_thread = threading.Thread(target=speedtest)
+
+kill_threads = False
+
+ping_thread.start()
+speedtest_thread.start()
+
+# Used to detect CTRL-C and terminate the threads
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    kill_threads = True
+
+ping_thread.join()
+speedtest_thread.join()
